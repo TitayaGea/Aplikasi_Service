423900355539525f48e49479ee19f80d7ef2548c
Titaya gea azrahra <geaazrahratitaya@gmail.com>
2020-05-04 21:12:26 +0700

    commit

---
+++ app/src/main/java/com/example/aplikasiservice/WallpaperChangeService.java
@@ -0,0 +1,61 @@
+package com.example.aplikasiservice;
+
+import android.app.Service;
+import android.app.WallpaperManager;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.content.Intent;
+import android.os.IBinder;
+import java.io.IOException;
+
+public class WallpaperChangeService extends Service implements Runnable{
+    private int wallpaperId[] = {R.drawable.satu, R.drawable.dua};
+    private int waktu;
+    private int FLAG=0;
+    private Thread t;
+    private Bitmap gambar;
+    private Bitmap gambar1;
+
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+    public int onStartCommand(Intent intent,int flag,int startId)
+    {
+        super.onStartCommand(intent,flag,startId);
+        Bundle bundle=intent.getExtras();
+        waktu=bundle.getInt("durasi");
+        gambar=BitmapFactory.decodeResource(getResources(),wallpaperId[0]);
+        gambar1=BitmapFactory.decodeResource(getResources(),wallpaperId[1]);
+
+        t = new Thread(WallpaperChangeService.this);
+        t.start();
+        return START_STICKY_COMPATIBILITY;
+
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        System.exit(0);
+    }
+    public void run(){
+        WallpaperManager myWallpaper;
+        myWallpaper=WallpaperManager.getInstance(this);
+        try{
+
+
+            while(true){
+                if(FLAG==0) {
+                    myWallpaper.setBitmap(gambar);
+                    FLAG++;
+                }
+                else{
+                    myWallpaper.setBitmap(gambar1);
+                    FLAG--;
+                }
+                Thread.sleep(100*waktu);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
